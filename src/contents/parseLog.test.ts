96bb56734fc02466121b8f8f42180668d26900e9
Peter_Lo <peter_lo@cmoney.com.tw>
2024-09-24 17:53:22 +0800

    Chore: add new test cases for parseCard

--- src/contents/parseLog.test.ts
+++ src/contents/parseLog.test.ts
@@ -1,11 +1,13 @@
 import { expect, test } from "vitest";
 import { JSDOM } from "jsdom";
 
-import { Action, parseLog } from "./parseLog";
+import { Category, Action, parseCard, parseLog } from "./parseLog";
 
+import heatCard from "/mockData/cards/heat.html?raw";
 import moveLog from "/mockData/logs/move.html?raw";
 import dropLog from "/mockData/logs/drop.html?raw";
 import playLog from "/mockData/logs/play.html?raw";
+import { describe } from "node:test";
 
 function convertToDom(htmlContent: string) {
   const dom = new JSDOM(htmlContent);
@@ -13,21 +15,30 @@ function convertToDom(htmlContent: string) {
   return document.body.firstElementChild as HTMLElement | null;
 }
 
-test("parse MOVE", () => {
-  const moveLogDom = convertToDom(moveLog);
-  expect(parseLog(moveLogDom)).toEqual({ action: Action.MOVE });
+describe("parseCard", () => {
+  test("parse HEAT card", () => {
+    const heatCardDom = convertToDom(heatCard);
+    expect(parseLog(heatCardDom)).toEqual({ category: Category.HEAT });
+  });
 });
 
-test("parse DROP", () => {
-  const dropLogDom = convertToDom(dropLog);
-  expect(parseLog(dropLogDom)).toEqual({ action: Action.DROP });
-});
+describe("parseLog", () => {
+  test("parse MOVE", () => {
+    const moveLogDom = convertToDom(moveLog);
+    expect(parseLog(moveLogDom)).toEqual({ action: Action.MOVE });
+  });
 
-test("parse PLAY", () => {
-  const playLogDom = convertToDom(playLog);
-  expect(parseLog(playLogDom)).toEqual({
-    action: Action.PLAY,
-    gear: 4,
-    cards: ['2', '4', '4', 'stress']
+  test("parse DROP", () => {
+    const dropLogDom = convertToDom(dropLog);
+    expect(parseLog(dropLogDom)).toEqual({ action: Action.DROP });
+  });
+
+  test("parse PLAY", () => {
+    const playLogDom = convertToDom(playLog);
+    expect(parseLog(playLogDom)).toEqual({
+      action: Action.PLAY,
+      gear: 4,
+      cards: ["2", "4", "4", "stress"],
+    });
   });
 });
--- src/contents/parseLog.ts
+++ src/contents/parseLog.ts
@@ -52,3 +52,27 @@ export function parseLog(dom: HTMLElement | null) {
     };
   }
 }
+
+/**
+ * 輸入範例
+ *  <div
+ *    class="log-card-image"
+ *    style="--personal-card-background-y: 66.66666666666667%"
+ *    data-symbols="2"
+ *  >
+ *    <div class="card personal-card" data-side="front">
+ *      <div class="card-sides">
+ *        <div
+ *          class="card-side front upgrade-card"
+ *          style="background-position: -100% -300%"
+ *        >
+ *          <div class="text">懸吊系統</div>
+ *        </div>
+ *      </div>
+ *    </div>
+ *  </div>
+ *
+ */
+export function parseCard(cardDom: HTMLElement | null) {
+  
+}
